9d6c6585d8443f8734d9aee4842b24e4f5c6e03d
Steven <zhangc2@illinois.edu>
2023-11-03 16:29:07 -0500

    updated features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -13,16 +13,16 @@ const Features: React.FC = () => {
         </div>
     <div className={styles.textArea}>
       <div className={styles.featureBlock}>
-        <h2>Easy Access to Resources</h2>
+        <h2>Easy Access to Education</h2>
         <p>Our platform provides a user-friendly interface that allows service people to easily access a wide range of educational resources, including scholarships, grants, and online courses.</p>
       </div>
       <div className={styles.featureBlock}>
-        <h2>Tailored Guidance</h2>
-        <p>We understand that each service person has unique educational goals and challenges. Our platform offers personalized guidance based on their unique background to help them navigate the higher education landscape and make informed decisions.</p>
+        <h2>AI-customized for You </h2>
+        <p>Based on your unique educational goals and challenges. Our platform offers personalized guidance to help them navigate the higher education landscape and make informed decisions.</p>
       </div>
       <div className={styles.featureBlock}>
-        <h2>Community Support</h2>
-        <p>Being part of a supportive community can greatly enhance the educational journey. Our platform connects service people with like-minded individuals, mentors, and experts who can provide valuable support and encouragement.</p>
+        <h2>Be part of a Community</h2>
+        <p>Our platform connects service people with like-minded individuals, mentors, and experts who can provide valuable support and share their journeys.</p>
       </div>
     </div>
     </div>

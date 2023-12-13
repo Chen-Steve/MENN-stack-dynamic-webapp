d900d3e9e4da6d7178e105d7baf2272a81b51451
Steven <zhangc2@illinois.edu>
2023-12-12 21:09:15 -0600

    fixed quotation w &quot;

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -139,7 +139,7 @@ const PrivacyPolicy: React.FC = () => {
                             <em><strong>In Short</strong>: Yes, we will update this notice as necessary to stay compliant with relevant laws.</em>
                         </p>
                         <p>
-                        We may update this privacy notice from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.
+                        We may update this privacy notice from time to time. The updated version will be indicated by an updated &quot;Revised&quot; date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.
                         </p>
                     </section>
 
